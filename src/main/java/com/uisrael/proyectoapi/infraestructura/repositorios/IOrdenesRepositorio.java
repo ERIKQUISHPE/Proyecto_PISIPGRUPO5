e4b558b1fbd88f99fc26fa5903866807bc88eac4
David Valencia <davidstevenvalencia@gmail.com>
2025-12-18 11:33:05 -0500

    Clase Ordenes "infraestructur.persistencia, infraestructura.repositorios"

---
+++ src/main/java/com/uisrael/proyectoapi/infraestructura/repositorios/IOrdenesRepositorio.java
@@ -0,0 +1,9 @@
+package com.uisrael.proyectoapi.infraestructura.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.uisrael.proyectoapi.infraestructura.persistencia.jpa.OrdenesJpa;
+
+public interface IOrdenesRepositorio extends JpaRepository<OrdenesJpa, Integer>{
+
+}
